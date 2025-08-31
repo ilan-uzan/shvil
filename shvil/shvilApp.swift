6d8a16b46c52892b38b5aff190ab38f9318aba57
ilan-uzan <ilanxuzan@gmail.com>
2025-08-31 23:40:57 +0300

    Initial Commit

---
+++ shvil/shvilApp.swift
@@ -0,0 +1,17 @@
+//
+//  shvilApp.swift
+//  shvil
+//
+//  Created by Ilan Uzan on 31/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct shvilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
